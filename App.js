d8179bb52e892266c4e4769763ee5a3232b73f1d
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2019-12-05 11:58:15 -0500

    pretty sure all dependancies are there. npm i

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import RestaurantList from './screens/RestaurantList'
+const AppNav = createStackNavigator ({
+  Home: {
+    screen: RestaurantList,
+    navigationOptions : {
+      title: 'Home',
+      headerStyle: {
+        backgroundColor: 'purple'
+      } 
+    }
+  }
+})
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(AppNav)
---
+++ screens/RestaurantList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class RestaurantList extends Component {
+    render() {
+        return (
+            <View>
+                <Text> This is gonna be the list. </Text>
+                <Text> This is gonna be the list. </Text>
+                <Text> This is gonna be the list. </Text>
+                <Text> This is gonna be the list. </Text>
+            </View>
+        )
+    }
+}
