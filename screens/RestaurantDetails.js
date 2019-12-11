be6349f8ac5c53f7a017498abdeea15e49c05034
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2019-12-11 13:56:11 -0500

    somewhat working...

--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native'
+import {AppLoading} from 'expo'
+import Carousel from 'react-native-banner-carousel'
 
 export default class RestaurantDetails extends Component {
     static navigationOptions = ({navigation}) => {
         return {title: navigation.state.params.restaurant.name}
     }
+    
     constructor(props){
         super(props)
         this.state = {
-            restaurant : this.props.navigation.state.params.restaurant,
-
+            restaurant : {
+                id : this.props.navigation.state.params.restaurant.id,
+                distance : this.props.navigation.state.params.restaurant.distance
+            },
+            extraRestaurantInfo : {},
+            ready: false
         }
     }
+
+    componentDidMount(){
+        this.fetchData()
+    }
+
     fetchData = () => {
     
-        const uri = `https://api.yelp.com/v3/businesses/${id}`
+        const uri = `https://api.yelp.com/v3/businesses/${this.state.restaurant.id}`
         const API_KEY = "k6tgobWytbIiwMCsLbyimwo27gJ-d1LO5SZn0XvAd_8dR5XOKUT2Sjpg0p2dc8SiylqFKa-TMF5l73d6z6IGI1bviHIbXAkZOv_KnZuIX_XsIimmfN_b3lJlFfDrXXYx"
         const authKey = `Bearer ${API_KEY}`
         let header = new Headers()
@@ -32,8 +44,10 @@ export default class RestaurantDetails extends Component {
             }
         })
         .then((data) => {
+            
+            console.log(data)
             this.setState({
-                businesses: data.businesses,
+                extraRestaurantInfo: data,
                 ready: true
             })
         })
@@ -41,15 +55,32 @@ export default class RestaurantDetails extends Component {
             alert(err)
         })
     }
-    render() {
+    renderPage(image, index) {
         return (
-            <View style={styles.container}>
-                <Text style={styles.name}> {this.state.restaurant.name} </Text>
-                <Text> {this.state.restaurant.location.display_address[0]} </Text>
-                <Text> {this.state.restaurant.location.display_address[1]} </Text>
-                <Text> {Math.floor(this.state.restaurant.distance)}m Away </Text>
+            <View key={index}>
+                <Image style={{ width: Banner.width, height: Banner.height }} source={{ uri: image }} />
             </View>
-        )
+        );
+    }
+    
+    render() {
+        if(!this.state.ready){
+            return <AppLoading/>
+        }else{
+            const images = this.state.extraRestaurantInfo.photos
+            
+            return (
+                <View style={styles.container}>
+                    <Carousel>
+                        {images.map((image, index) => this.renderPage(image, index))}   
+                    </Carousel>
+                    <Text style={styles.name}> {this.state.extraRestaurantInfo.name} </Text>
+                    <Text> {this.state.extraRestaurantInfo.location.display_address[0]} </Text>
+                    <Text> {this.state.extraRestaurantInfo.location.display_address[1]} </Text>
+                    <Text> {Math.floor(this.state.restaurant.distance)}m Away </Text>
+                </View>
+            )
+        }
     }
 }
 
